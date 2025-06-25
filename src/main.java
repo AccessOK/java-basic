2f6bd66099a731eb495cc245b90f829e9af11e0a
AccessOK <accessok@163.om>
2025-06-25 09:21:13 +0800

    ad

---
+++ src/main.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+//        Integer num1=127;
+//        Integer num2=127;
+//        if(num1==num2){
+//            System.out.println("Equal");
+//        }
+//        else{
+//            System.out.println("Not Equal");
+//        }
+//        if(num1.equals(num2)){
+//            System.out.println("Equal1");
+//        }
+//        else{
+//            System.out.println("Not Equal1");
+//        }
+        String str= null;
+        StringBuffer sb= new StringBuffer();
+        sb.append(str);
+        System.out.println(sb);
+    }
+}
