40f4c06e4c5200b6989d53b3d4ac42708b03b057
AccessOK <wangjing@uniontech.com>
2025-07-15 13:10:02 +0800

    add 3-10

--- src/main.java
+++ src/main.java
@@ -5,14 +5,37 @@ import java.nio.file.Path;
 import java.util.Scanner;
 
 class Main {
+    //main函数接收一个字符串数组作为参数
     public static void main(String[] args) {
-        if(args.length==0||args[0].equals("--help")){
-            System.out.println("Usage: java Main [--help] [--input <file>] [--output <file>]");
+        final double STARTRATE = 10;
+        final int NRATES = 6;
+        final int NYEARS = 10;
+        double[] interestRate = new double[NRATES];
+        //定义单元行数据
+        for (int i = 0; i < interestRate.length; i++) {
+            interestRate[i] = (STARTRATE + i) / 100.0;
         }
-        else if(args[0].equals("-g")){
-            System.out.println("Quiet moe");
-
+        double[][] balances = new double[NYEARS][NRATES];
+        //二维数组第一行数据填充
+        for (int i = 0; i < interestRate.length; i++){
+            balances[0][i] = 10000;
         }
-
+        for (int i = 1; i < balances.length; i++) {
+            for (int j = 0; j < balances[i].length; j++) {
+                double oldBalance = balances[i-1][j];
+                double interest = oldBalance * interestRate[j];
+                balances[i][j] = oldBalance + interest;
+            }
+        }
+        //输出一行的数据
+        for (int i = 0; i < interestRate.length; i++){
+            System.out.printf("%9.0f%%", interestRate[i] * 100);
+        }
+        for (double[] row : balances) {
+            for (double b: row){
+                System.out.printf("%10.2f", b);
+            }
+            System.out.println();
+        }
     }
 }
