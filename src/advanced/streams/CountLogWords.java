2012cea11e6e21f38ef9442d5ac67754feaac4e9
wangjing <wangjing@uniontech.com>
2025-09-04 18:35:29 +0800

    add II

--- src/advanced/streams/CountLogWords.java
+++ src/advanced/streams/CountLogWords.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class CountLogWords{
     public static void main(String[] args) throws IOException{
-        var contents=Files.readString(Path.of("./words.txt"));
+        var contents=Files.readString(Path.of("./src/advanced/streams/words.txt"));
         List<String> words=List.of(contents.split("\\PL+"));
         long count=0;
         for(String word:words){
@@ -18,5 +20,20 @@ public class CountLogWords{
             count=words.parallelStream().filter(w -> w.length()>4).count();
             System.out.println(count);
         }
+        //数组转换成流
+        var a=Stream.of("asd","weqw","asdweqwe").toList();
+        System.out.println(a);
+        //创建无限流，通过limit()限制流大小
+        var b=Stream.generate(()->{return "asd";}).limit(2).toList();
+        System.out.println(b);
+        //创建无限流，若不限制大小，会超内存
+        var c=Stream.generate(Math::random).limit(20).toList();
+        System.out.println(c);
+        // 无限流
+        var d=Stream.iterate(BigInteger.ZERO, n->n.add(BigInteger.ONE)).limit(10).toList();
+        System.out.println(d);
+        //限制流
+        var e=Stream.iterate(BigInteger.ZERO, n->n.compareTo(new BigInteger("102"))<0,n->n.add(BigInteger.ONE)).toList();
+        System.out.println(e);
     }
 }
---
+++ src/advanced/streams/CreatingStreams.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class CreatingStreams {
+    public static <T> void show(String title,Stream<T> stream){
+        final int SIZE=10;
+        List<T> firstElements= stream.limit(SIZE+1).toList();
+        System.out.println(title);
+    }
+    public static void main(String args[]) throws IOException{
+        Path path=Path.of("./src/advanced/streams/words.txt");
+        var contents = Files.readString(path);
+        Stream<String> words=Stream.of(contents.split("\\PL+"));
+        show("words", words);
+
+    }
+}
